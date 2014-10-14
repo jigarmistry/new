8be8410de598434f363b09f716c00f9cb59dfaf0
Jigar Mistry <jigarmistry24@gmail.com>
2014-10-14 21:38:33 +0530

    Remove spaces and unwanted comments

--- PythonExamples/list1.py
+++ PythonExamples/list1.py
@@ -1,17 +1,14 @@
 def intersect(seq1, seq2):
-    res = []                     # start empty
-    for x in seq1:               # scan seq1
-        if x in seq2:            # common item?
-            res.append(x)        # add to end
+    res = []             
+    for x in seq1:       
+        if x in seq2:    
+            res.append(x)
     return res
 
 s1 = "JIGAR"
 s2 = "MISTRY"
 
-print intersect(s1, s2)                   # strings
-
+print intersect(s1, s2)
 s1 = [1,2,3,4,5]
 s2 = [3,4,5,6,7]
-
-
 print intersect(s1, s2)  
